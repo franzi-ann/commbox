8f99b30d2b6b9be3970b932e293a38c4ce272c72
fbecker <franziska.becker@congstar.net>
2022-12-28 16:15:06 +0100

    Doku

--- src/main/java/de/fomwebtech/servlet/postbox/PostboxGetEntities.java
+++ src/main/java/de/fomwebtech/servlet/postbox/PostboxGetEntities.java
@@ -14,7 +14,7 @@ import de.fomwebtech.database.statements.SQLStatementsPostbox;
 import de.fomwebtech.user.User;
 
 
-
+/*Diese Klasse dient dazu die Liste aller Kommunikationen eines Users bereitzustellen.*/
 @WebServlet(urlPatterns = ("/service/postboxEntities"))
 public class PostboxGetEntities extends HttpServlet {
     private static final long serialVersionUID = 5652268371771279360L;
